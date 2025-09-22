886769162faec35c0316cb318d532f916c312421
milenamileva10-afk <milenamileva10@gmail.com>
2025-09-22 10:47:33 +0200

    Update test_package.R

--- test_package.R
+++ test_package.R
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
 library(DBI)
-library(tidyverse)# "Hello world"
-
-=======
-library (DBI)
-library (tidyverse) #Hello 
->>>>>>> 07b638c13b7d906e0246d1424dad31a4b913950a
+library(tidyverse)# "Hello world again
